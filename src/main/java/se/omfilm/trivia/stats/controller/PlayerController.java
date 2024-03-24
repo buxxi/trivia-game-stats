f8d0ab6c259d291fbc3bbe3a04082f7f0bf5e4f2
buxxi <buxxi@omfilm.net>
2024-03-24 15:33:38 +0100

    Implement a basic alias handling

--- src/main/java/se/omfilm/trivia/stats/controller/PlayerController.java
+++ src/main/java/se/omfilm/trivia/stats/controller/PlayerController.java
@@ -3,12 +3,11 @@ package se.omfilm.trivia.stats.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se.omfilm.trivia.stats.controller.io.PlayerDetailsResponse;
 import se.omfilm.trivia.stats.controller.io.PlayerSummaryResponse;
+import se.omfilm.trivia.stats.service.PlayerAliasService;
 import se.omfilm.trivia.stats.service.PlayerService;
 
 import java.util.List;
@@ -16,10 +15,12 @@ import java.util.List;
 @RestController
 public class PlayerController {
     private final PlayerService playerService;
+    private final PlayerAliasService playerAliasService;
 
     @Autowired
-    public PlayerController(PlayerService playerService) {
+    public PlayerController(PlayerService playerService, PlayerAliasService playerAliasService) {
         this.playerService = playerService;
+        this.playerAliasService = playerAliasService;
     }
 
     @GetMapping(value = "/players", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,5 +33,15 @@ public class PlayerController {
         return playerService.getPlayerDetails(name)
                 .map(PlayerDetailsResponse::new)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No player with name: " + name));
+    }
+
+    @PutMapping(value = "/players/{name}/alias")
+    public void addAlias(@PathVariable("name") String name, @RequestBody List<String> aliases) {
+        playerAliasService.addAliases(name, aliases);
+    }
+
+    @DeleteMapping(value = "/players/{name}/alias")
+    public void deleteAlias(@PathVariable("name") String name, @RequestBody List<String> aliases) {
+        playerAliasService.removeAliases(name, aliases);
     }
 }
--- src/main/java/se/omfilm/trivia/stats/service/PlayerAliasService.java
+++ src/main/java/se/omfilm/trivia/stats/service/PlayerAliasService.java
@@ -2,16 +2,68 @@ package se.omfilm.trivia.stats.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Stream;
 
 @Service
 public class PlayerAliasService {
+    private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private static final Map<String, List<String>> ALIAS_MAP = new HashMap<>(); //TODO: make actual infrastructure
+
     public Optional<String> getMainName(String gameName) {
-        return Optional.empty();
+        lock.readLock().lock();
+        try {
+            return resolveAlias(gameName);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public List<String> getAliases(String mainName) {
-        return List.of();
+        lock.readLock().lock();
+        try {
+            return ALIAS_MAP.getOrDefault(mainName, List.of());
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public void addAliases(String name, List<String> aliases) {
+        lock.writeLock().lock();
+        try {
+            if (isAlias(name)) {
+                throw new IllegalStateException(name + " is already used as an alias");
+            }
+            ALIAS_MAP.merge(name, aliases, (a, b) -> Stream.concat(a.stream(), b.stream()).toList());
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public void removeAliases(String name, List<String> aliases) {
+        lock.writeLock().lock();
+        try {
+            if (isAlias(name)) {
+                throw new IllegalStateException(name + " is already used as an alias");
+            }
+            ALIAS_MAP.put(name, ALIAS_MAP.getOrDefault(name, List.of()).stream().filter(alias -> !aliases.contains(alias)).toList());
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private boolean isAlias(String name) {
+        return resolveAlias(name).isPresent();
+    }
+
+    private Optional<String> resolveAlias(String gameName) {
+        return ALIAS_MAP.entrySet().stream()
+                .filter(e -> e.getValue().contains(gameName))
+                .map(Map.Entry::getKey)
+                .findAny();
     }
 }
